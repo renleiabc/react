90692d8e3519d38bc5423d86fe2bfc60ec818581
renleiabc <421796896@qq.com>
2019-02-15 17:23:26 +0800

    renlei

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # react
 react学习
+在create-react-app脚手架当中使用sass注意事项
+1、下载
+npm install node-sass sass-loader --save-dev
+2、在开发阶段，找到项目的node_modules/react-scripts/config文件夹下找到webpack.config.js文件
+在webpack.config.js文件中，找到rule下的oneof在里面添加
+  //sass
+          {
+            test: /\.scss$/,
+            loaders: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap'],
+          },
+这里style，css和sass这几个loader的功能都比较直观，我们知道"?sourceMap"这个语法的意思是开启对应模块的sourceMap属性，
+那么这个sourceMap又是干什么用的呢？
+其实这里主要的目的就是方便我们调试sass代码。因为sass代码最终还是要编译成css的，而我们在调试的时候，
+我们更愿意在Chrome的开发者工具中能够直接看到我们的sass代码来进行调试。
+（注意：在使用npm run build打包上传之前，要把上述代码删除，否则会把css打包进js当中）
+3、在打包之前删除webpack.config.js文件中
+devtool: isEnvProduction
+      ? shouldUseSourceMap
+        ? 'source-map'
+        : false
+      : isEnvDevelopment && 'cheap-module-source-map',
+      以便去除js当中的map文件；
--- simple/src/App.js
+++ simple/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import asyncComponent from "./asyncComponent"
+import asyncComponent from './asyncComponent'
 //通过asyncComponent导入组件， 创建代码分片点
-const AsyncHome = asyncComponent(() => import("./components/Home"))
-const AsyncLogin = asyncComponent(() => import("./components/Login"))
-
+const AsyncHome = asyncComponent(() => import('./components/Home'))
+const AsyncLogin = asyncComponent(() => import('./components/Login'))
 
 class App extends Component {
   render() {
-    return <Router>
+    return (
+      <Router>
         <Switch>
-            <Route exact path="/" component={AsyncHome} />
-            <Route path="/login" component={AsyncLogin} />
-            <Route path="/posts" component={AsyncHome} />
+          <Route exact path="/" component={AsyncHome} />
+          <Route path="/login" component={AsyncLogin} />
+          <Route path="/posts" component={AsyncHome} />
         </Switch>
       </Router>
+    )
   }
 }
 
-export default App;
+export default App
