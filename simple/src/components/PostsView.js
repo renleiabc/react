8845215dab90dc3bb59564d6cbc9f5e1863bee2e
renleiabc <421796896@qq.com>
2020-05-23 16:01:16 +0800

    renlei

--- simple/src/components/PostsView.js
+++
@@ -1,7 +0,0 @@
-/*
- * @Author: renlei
- * @Date: 2020-05-21 18:07:43
- * @LastEditors: renlei
- * @LastEditTime: 2020-05-21 18:08:39
- * @Description:渲染帖子内容组件
- */
--- simple/src/page/postEditor.js
+++
@@ -1,7 +0,0 @@
-/*
- * @Author: renlei
- * @Date: 2020-05-21 18:07:59
- * @LastEditors: renlei
- * @LastEditTime: 2020-05-21 18:08:17
- * @Description:发帖
- */
